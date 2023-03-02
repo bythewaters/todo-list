c6025a3094f4edf1c5d469ea4baad371b1878aa8
Taras Martyniuk <bythewaters800@gmail.com>
2023-03-02 16:37:12 +0100

    Added models for my_todo_list app

--- my_todo_list/models.py
+++ my_todo_list/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=63)
+
+
+class Task(models.Model):
+    content = models.CharField(max_length=255)
+    date_time = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField()
+    mark = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, related_name="task")
