39dd4d3de9eb5db4a37760570d68308085830a31
Taras Martyniuk <bythewaters800@gmail.com>
2023-03-03 23:34:18 +0100

    Added TagListView & index function

--- my_todo_list/views.py
+++ my_todo_list/views.py
@@ -1,3 +1,13 @@
+from django.views import generic
 from django.shortcuts import render
 
-# Create your views here.
+from my_todo_list.models import Tag
+
+
+def index(request):
+    return render(request, "my_todo_list/task.html")
+
+
+class TagListView(generic.ListView):
+    model = Tag
+    template_name = "my_todo_list/tag.html"
