c7dd4c5ab59faaa3e22b7f2cce99779eb85ebfad
Taras Martyniuk <bythewaters800@gmail.com>
2023-03-03 23:36:02 +0100

    Added url for TagListView

--- my_todo_list/urls.py
+++ my_todo_list/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-urlpatterns = [
+from my_todo_list.views import index, TagListView
 
+urlpatterns = [
+    path("", index, name="index"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
 ]
 
 app_name = "my_todo_list"
