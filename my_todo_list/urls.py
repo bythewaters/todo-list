b20e491007e52b491bce7206c29e3d5414a5c5a6
Taras Martyniuk <bythewaters800@gmail.com>
2023-03-04 22:04:02 +0100

    fix path

--- my_todo_list/urls.py
+++ my_todo_list/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 
-from my_todo_list.views import TagListView, TagCreateView, TagUpdateView, TagDeleteView, TaskListView, TaskDeleteView, \
-    TaskUpdateView
+from my_todo_list.views import (
+    TagListView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView,
+    TaskListView,
+    TaskDeleteView,
+    TaskUpdateView,
+    TaskCreateView
+)
 
 urlpatterns = [
     path("tags/", TagListView.as_view(), name="tag-list"),
@@ -10,7 +18,8 @@ urlpatterns = [
     path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
     path("", TaskListView.as_view(), name="task-list"),
     path("task/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
-    path("task/<int:pk>/delete/", TaskUpdateView.as_view(), name="task-update"),
+    path("task/create/", TaskCreateView.as_view(), name="task-create"),
+    path("task/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
 ]
 
 app_name = "my_todo_list"
