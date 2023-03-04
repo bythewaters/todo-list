c8580bfff41147d0270d0183f39423ec917be078
Taras Martyniuk <bythewaters800@gmail.com>
2023-03-04 19:36:28 +0100

    Added home page how TaskList

--- my_todo_list/urls.py
+++ my_todo_list/urls.py
@@ -6,11 +6,11 @@ from my_todo_list.views import TagListView, TagCreateView, TagUpdateView, TagDel
 urlpatterns = [
     path("tags/", TagListView.as_view(), name="tag-list"),
     path("tags/create/", TagCreateView.as_view(), name="tag-create"),
-    path("tags/<int:pk>/update", TagUpdateView.as_view(), name="tag-update"),
-    path("tags/<int:pk>/delete", TagDeleteView.as_view(), name="tag-delete"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
     path("", TaskListView.as_view(), name="task-list"),
-    path("task/<int:pk>/delete", TaskDeleteView.as_view(), name="task-delete"),
-    path("task/<int:pk>/delete", TaskUpdateView.as_view(), name="task-update"),
+    path("task/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path("task/<int:pk>/delete/", TaskUpdateView.as_view(), name="task-update"),
 ]
 
 app_name = "my_todo_list"
