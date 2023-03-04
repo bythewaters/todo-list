85496c62be00e8ab69dec5b360e7f21a62dcc108
Taras Martyniuk <bythewaters800@gmail.com>
2023-03-04 22:03:13 +0100

    Added crispy forms in Installed apps

--- Todo_list/settings.py
+++ Todo_list/settings.py
@@ -37,8 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'my_todo_list'
+    'crispy_forms',
+    'crispy_bootstrap4',
+    'my_todo_list',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
