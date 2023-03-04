8286815f1e166d3146a5aa0d9c126adbecc577af
Taras Martyniuk <bythewaters800@gmail.com>
2023-03-04 22:03:42 +0100

    Added str representation in tag model

--- my_todo_list/models.py
+++ my_todo_list/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Tag(models.Model):
     name = models.CharField(max_length=63)
 
+    def __str__(self) -> str:
+        return f"{self.name}"
+
 
 class Task(models.Model):
     content = models.CharField(max_length=255)
