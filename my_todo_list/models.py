945432679f7880f7bde61aa8df9bf116cbce31b4
Taras Martyniuk <bythewaters800@gmail.com>
2023-03-07 14:29:36 +0100

    Fix: cannot added task without deadline

--- my_todo_list/models.py
+++ my_todo_list/models.py
@@ -11,7 +11,7 @@ class Tag(models.Model):
 class Task(models.Model):
     content = models.CharField(max_length=255)
     date_time = models.DateTimeField(auto_now_add=True)
-    deadline = models.DateTimeField()
+    deadline = models.DateTimeField(blank=True, null=True)
     mark = models.BooleanField(default=False)
     tags = models.ManyToManyField(Tag, related_name="task")
 
